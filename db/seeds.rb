7702b69c28db8a4f0c80326f0af05d1f75f39c4f
Student <example@example.com>
2019-08-20 16:46:14 -0700

    Add API authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
 class Seed
   def self.begin
     Destination.destroy_all
+    Review.destroy_all
     seed = Seed.new
     seed.generate_destinations
   end
   def generate_destinations
     25.times do |i|
       destination = Destination.create!(
+        destination: Faker::Games::Pokemon.location,
         city: Faker::Nation.capital_city,
         country: Faker::Games::Pokemon.location
       )
